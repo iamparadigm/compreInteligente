2018536b60c6b6352a99a446af305f32da3d23fe
Nathalia Ferres <nana_cmf@hotmail.com>
2024-07-16 21:14:31 -0300

    criando a classe de parcelamento no javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+class calculadoParcelamento {
+    constructor (produto, nome, valor, memoria, parcelas) {
+        this.produto = produto;
+        this.nome = nome;
+        this.valor = valor;
+        this.memoria = memoria;
+    }
+
+parcelas1() {
+    if (this.parcela != 0 && <= 5; || this.parcela >= 6 && >=10; || >= 11 && <= 15) 
+
+alert(`${} parcelas de x valor`)
+}
+
+numeroDeParcelas() => {
+
+}
+
+
+
+
+} 
+
+
+
