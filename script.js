6d1c6c24e8dc80949e6d4398154e69bae6499aa3
Nathalia Ferres <nana_cmf@hotmail.com>
2024-07-17 12:15:10 -0300

    inclusão de novos metodos na calculadora de parcelamento

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 class calculadoParcelamento {
-    constructor (produto, nome, valor, memoria, parcelas) {
-        this.produto = produto;
-        this.nome = nome;
-        this.valor = valor;
-        this.memoria = memoria;
+    constructor (valorTotal, taxaDeJuros, numeroDeParcelas) {
+        this.valorTotal = valorTotal;
+        this.taxaDeJuros = taxaDeJuros;
+        this.numeroDeParcelas = numeroDeParcelas;
+        this.jurosPorParcela();
+        
     }
 
-parcelas1() {
-    if (this.parcela != 0 && <= 5; || this.parcela >= 6 && >=10; || >= 11 && <= 15) 
-
-alert(`${} parcelas de x valor`)
+jurosPorParcela() {
+    if (this.numeroDeParcelas <= 5){
+        this.taxaDeJuros = 5;
+    } else if (this.numeroDeParcelas >= 6 && this.numeroDeParcelas <=10) {
+        this.taxaDeJuros = 7;
+        
+    }else {
+        (this.numeroDeParcelas >= 11 && <= 15 )
+        this.taxaDeJuros = 10
+    }
 }
 
-multiplicacaoDaParcela() => {
-this.valor = this.valor * 5
+calcularParcela () {
+    const taxaMensal = this.taxaDeJuros / 12 / 100;
+    const valorParcela = this.valorTotal * taxaMensal / (1 - Math.pow(1 + taxaMensal, - this.numeroDeParcelas ));
+    return (valorParcela.toFixed(2));
 }
-
 
 
 
